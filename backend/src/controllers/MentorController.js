53ee17ca0364f3305ddd61216db05585efa22e25
Ganesh140404 <polaganesh1414@gmail.com>
2025-09-26 13:25:11 +0530

    corrected mentor getProfile1

--- backend/src/controllers/MentorController.js
+++ backend/src/controllers/MentorController.js
@@ -27,6 +27,9 @@ export const getMentorProfile=async(req,res)=>{
         })
     } catch (error) {
         console.log(error)
+        return res.status(500).json({
+            message:"server error"
+        })
     }
 
 }
